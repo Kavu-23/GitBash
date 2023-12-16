d287437c18cfa8c5f08de8708bf938ca220b598e
DELL <DELL@DESKTOP-5DJG0VO>
2023-12-16 20:09:10 +0530

    Test Done

---
+++ src/test/java/PageTest/ProductTest.java
@@ -0,0 +1,14 @@
+package PageTest;
+
+import org.testng.annotations.Test;
+
+public class ProductTest {
+
+	@Test
+	public void  productPagetest()
+	
+	{
+	  System.out.println("Hello");	
+      System.out.println("Product created");
+	}
+}
